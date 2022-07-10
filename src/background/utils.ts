ad3155138f49f973f77c50a121bdfe8457fb176a
Vlad Brok <vlad.brok99@gmail.com>
2022-07-11 00:00:23 +0300

    migrated getTabsInCurrentWindow method to traditional for loop

--- src/background/utils.ts
+++ src/background/utils.ts
@@ -37,20 +37,22 @@ export async function getTabIdWithSearchOpen(
 export async function getTabsInCurrentWindow() {
   // should it return the current tab??
   const tabs = await browser.tabs.query({ currentWindow: true });
-  return tabs
-    .filter((tab) => {
-      // will all pages have a title?
-      return tab.id && tab.id !== browser.tabs.TAB_ID_NONE && tab.url;
-    })
-    .map(({ id, favIconUrl, title, url }) => {
+  const results: TabData[] = [];
+
+  for (let i = 0; i < tabs.length; i++) {
+    // will all pages have a title?
+    if (tabs[i].id && tabs[i].id !== browser.tabs.TAB_ID_NONE && tabs[i].url) {
       // we know that these properties will be present
       const tabData: TabData = {
-        tabId: id!,
-        favIcon: favIconUrl || null,
-        tabTitle: title!,
-        tabUrl: url!,
+        tabId: tabs[i].id!,
+        favIcon: tabs[i].favIconUrl || null,
+        tabTitle: tabs[i].title!,
+        tabUrl: tabs[i].url!,
         // muted info
       };
-      return tabData;
-    });
+      results.push(tabData);
+    }
+  }
+
+  return results;
 }
