4a2d1007d9eac55d2575716ec24800e49c1567aa
Mitchell Mark-George <mitchellmmarkgeorge@gmail.com>
2022-07-23 15:32:40 -0400

    added ZipPlugin for production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,10 +3,23 @@ const common = require("./webpack.config.js");
 const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
+const path = require("path")
+
+let isFirefox = process.env.BROWSER === "firefox";
+
+const zipFileName = isFirefox ? "firefox.zip" : "chrome.zip";
+
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ZipPlugin({
+      path: path.resolve(__dirname, "dist"),
+      filename: zipFileName,
+    }),
+  ],
   optimization: {
     minimizer: [
       new TerserPlugin({
