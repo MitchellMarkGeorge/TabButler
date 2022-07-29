bffcf19abfe6a0d01cd1abbc9b3bea2ac7992337
Mitchell Mark-George <mitchellmmarkgeorge@gmail.com>
2022-07-29 14:22:57 -0400

    aliased react to preact

--- webpack.config.js
+++ webpack.config.js
@@ -65,5 +65,11 @@ module.exports = {
 
   resolve: {
     extensions: [".js", ".json", ".ts", ".tsx"],
+    alias: {
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",     // Must be below test-utils
+      "react/jsx-runtime": "preact/jsx-runtime"
+    }
   },
 };
